b9350527744995a58a38dbc5308ef2c6f72eec63
Chilon57 <lopezowie57@interia.pl>
2018-04-18 10:54:26 +0200

    #14 uploading data (without displaying)

--- GrzegorzWojnarowicz/skrypty.js
+++ GrzegorzWojnarowicz/skrypty.js
@@ -1,12 +1,25 @@
 class Klasa{
     constructor(fetchURL){
-        this.fetchURL = 'github.com/axios/axios'
+        
+        this.fetchURL = 'http://dulian.cartodb.com/api/v2/sql/?q=SELECT%20*%20FROM%20mddk_info'
+    }
+    
+    pobierz()
+    {
+        axios.get(this.fetchURL)
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+
+    wywolaj(){
+        
     }
 }
-fetch('http://dulian.cartodb.com/api/v2/sql/?q=SELECT%20*%20FROM%20mddk_info')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function() {
-    console.log();
-  });
+
+var transport = new Klasa;
+
