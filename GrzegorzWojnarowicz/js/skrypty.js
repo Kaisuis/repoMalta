bc2ac8e144ebdea257d1057a8f917d12f049c128
Chilon57 <lopezowie57@interia.pl>
2018-04-19 11:15:16 +0200

    #17 links working but zoom on map is huge

--- GrzegorzWojnarowicz/js/skrypty.js
+++ GrzegorzWojnarowicz/js/skrypty.js
@@ -21,11 +21,11 @@ class Klasa{
         
         let lista = '<ul>';
         response.data.rows.forEach((element) => {
-            let koordynaty = this.filtrowanie(element);
+            let link = this.filtrowanie(element);
             lista += '<li>' + element.adres + '<br>' + element.autorka + '<br>' + element.ceny_biletow + '<br>' + element.dostepnosc + '<br>' + 
             element.e_mail + '<br>' + '<a target="_blank" href="' + [element.flickr] + '"> zdjęcie </a>' + '<br>' + element.gmina + '<br>' + element.godziny_otwarcia + '<br>' + 
             element.gps + '<br>' + element.obiekt + '<br>' + element.opis + '<br>' + element.longitude_0 + '<br>' + element.latitude_0 + '<br>' + '<a target="_blank" href="' + 
-            [element.link] + '"> mapa </a>' + '</li>';
+            link + '"> mapa </a>' + '</li>';
         });
 
      
@@ -42,14 +42,13 @@ class Klasa{
 
         if(element.hasOwnProperty("latitude_0") && element.hasOwnProperty("longitude_0") && element.longitude_0 != null && element.longitude_0 != "" && element.latitude_0 != null && element.latitude_0 != "")
             {
-                var dana = element.longitude_0;
-                var dana2 = element.latitude_0
+                var dana = parseFloat(element.longitude_0).toFixed(4);
+                var dana2 = parseFloat(element.latitude_0).toFixed(4);
                 //console.log(dana);
               //  console.log(dana2);
                 link = "https://www.gps-coordinates.net/latitude-longitude/"+dana+"/"+dana2+"/10/roadmap";
             
             }
-
         return link;
             
     }
