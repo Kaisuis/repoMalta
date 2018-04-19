0af04169e1edf7faee844318c0bc7eda1456af0e
Kaisuis <stasiu.musial19@gmail.com>
2018-04-19 16:46:57 +0200

    #18 pobranie do console danych vue.js

--- Kaisuis/js/script.js
+++ Kaisuis/js/script.js
@@ -1,6 +1,29 @@
 new Vue({
     el: '#app',
-    data: {
-        message: 'Greetings your Majesty!'
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted(){
+        this.initData();
+       },
+       methods:{
+        initData(){
         }
+       }
+      })
+
+      
+      
+axios.get('http://dulian.cartodb.com/api/v2/sql/?q=SELECT%20*%20FROM%20mddk_info')
+      .then((response)=>{
+      console.log(response)
     })
+    .catch((error)=>{
+    })
+    .then((response)=>{
+        
+        this.info = response.rows.obiekt
+
+      })
